c81f24c3c40f7f5fecd5ea40893b89b51bdf95c8
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-03-31 10:54:28 -0500

    JS Project Basics
    basic HTML to run JS. JS includes alerts, window.alert, document.write, changing font color, expression, and concatenating strings

---
+++ Project_Expressions_Alert/JS/main.js
@@ -0,0 +1,27 @@
+alert("CLICK 'OK' TO REMOVE THIS BOX"); // alert box
+
+var B = "Hello there!";
+
+window.alert(B); // window displaying var B
+
+var A = "Here I am!";
+
+document.write(A); // write var A on the page
+
+var cat = 'I\'m a cat.';
+
+var dog = ' I am not a dog.';
+
+alert(cat + dog); // alert that concatenates string variables
+
+var dad="David", mom="Stacy", brother="Kyle", me="Adam";
+
+var blues = brother.fontcolor("blue"); // var that makes brother blue
+
+document.write(' ' + blues); // write blue brother on page
+
+var math = 3 + 3; // expression
+
+alert("What is 3 + 3?");
+
+alert(`It is ${math}!`); // printing variable that had expression as a string even thoughj it is an integer, ${} does that for you
