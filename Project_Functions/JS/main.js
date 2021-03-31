f8e6ac8819b7f531049049891d17832f6b9d6b51
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-03-31 11:31:32 -0500

    Event JS and Image
    More event functionality and JS functions. Image added for HTML event

--- Project_Functions/JS/main.js
+++ Project_Functions/JS/main.js
@@ -13,3 +13,13 @@ function concat() {
     sent += " Adam.";
     document.getElementById("concat").innerHTML = sent;
 }
+
+function normalImg(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+}
+
+function bigImg(x) {
+    x.style.height = "128px";
+    x.style.width = "128px";
+}
