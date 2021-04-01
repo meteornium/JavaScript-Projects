74a7ad79d7e7246c32ea7e020ad81228513229ea
Adam Kelley <79771326+meteornium@users.noreply.github.com>
2021-04-01 16:33:47 -0500

    Comments
    added more comments

--- Basic JavaScript Projects/Project_Countdown_Slideshow/JS/main.js
+++ Basic JavaScript Projects/Project_Countdown_Slideshow/JS/main.js
@@ -17,24 +17,26 @@ function countdown() {
 var slideIndex = 1;
 showSlides(slideIndex);
 
+//function that moves slide up one
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+// this function controls which slide the show starts on
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  var i; // declaring variable to be used as a counter
+  var slides = document.getElementsByClassName("mySlides"); // setting slide equal to whatever slide we are on
+  var dots = document.getElementsByClassName("dot"); // same as last step but with the dots
+  if (n > slides.length) {slideIndex = 1} // saying that if our index is greater than the number of slides, go to slide one
+  if (n < 1) {slideIndex = slides.length} // sets index equal to the last slide if n is lest than one
+  for (i = 0; i < slides.length; i++) { // let i, the counter, start at 0. while i is less than the length of our slides (we have 3 slides, so our array goes to index 2), then add 1 to i after every loop
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
+  for (i = 0; i < dots.length; i++) { // same as previous but with the dots
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block";  
